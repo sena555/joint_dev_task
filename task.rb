72ff5febd922918ebbd9f1099a3ca76e78b85d1e
sena555 <sayaka095@gmaii.com>
2020-03-15 23:36:56 +0900

    質問6〜9に回答

--- task.rb
+++ task.rb
@@ -48,21 +48,24 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+ numbers2 = numbers1.map { |n| n*10 }
+  puts numbers2
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+　array = array.map!(&:to_i)
+  puts array
 end
 
 def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map! { |item| item.capitalize }
+  upper_case_programming_languages  = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -72,6 +75,7 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names.each.with_index(1) { |name, i| puts "会員No.#{i} #{name}さん" } 
 
 end
 
