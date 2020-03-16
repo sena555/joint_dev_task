d494ce1ed59d398fbaf5e71b16b2d9365b947ea4
sena555 <sayaka095@gmaii.com>
2020-03-16 22:46:55 +0900

    質問6,8を修正

--- task.rb
+++ task.rb
@@ -48,8 +48,7 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
- numbers2 = numbers1.map { |n| n*10 }
-  puts numbers2
+  puts number2 = numbers1.map { |x| x * 10 }
 end
 
 def q7
@@ -64,7 +63,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages.map! { |item| item.capitalize }
+  programming_languages.map!(&:capitalize)
   upper_case_programming_languages  = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
